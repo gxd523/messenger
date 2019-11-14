517f51f123650e82d32a303c66cb45e4af18ca21
gxd <guoxiaodong@haqu.com>
2019-11-14 14:09:50 +0800

    nothing

--- client/src/main/java/com/demo/messenger/client/MainActivity.java
+++ client/src/main/java/com/demo/messenger/client/MainActivity.java
@@ -74,6 +74,7 @@ public class MainActivity extends Activity {
         @Override
         public void handleMessage(Message msg) {
             Bundle bundle = msg.getData();
+            // 这段话用于获取客户端发送过来的Book信息，难点在于bundles为什么要设置当前环境的类的加载器，前面说过，Messenger远程通信，归根到底是Parcel在起作用，因此传递的对象必须是实现了Parcelable接口的对象，而ClassLoader类并没有实现，因此在传递过程中会丢失，所以需要重新加载当前环境的类的加载器去寻找User类别进行实例化。如果不设置，会报ClassNotFoundException错误。如果我们把上面的Log语句的注释去掉，系统会报空指针异常，很好的证明了ClassLoader类的消息并不能跨进程传输。
             bundle.setClassLoader(Thread.currentThread().getContextClassLoader());
             MsgData msgData = bundle.getParcelable("server");
             Log.d("gxd", "客户端收到了消息-->" + msgData);
